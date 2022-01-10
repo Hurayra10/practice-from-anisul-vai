1f12174d69424f89b703572a41e9f30349386999
hurayra10 <abuhurayrapust10@gmail.com>
2022-01-10 15:55:56 +0600

    changing style by js done

---
+++ changingCssStyle.js
@@ -0,0 +1,10 @@
+
+
+function addStyle() {
+   var myvar = document.querySelector("#paraId");
+   myvar.style.color = "red";
+    myvar.style.fontSize = "3rem"
+    myvar.style.fontWeight = "boid"
+    myvar.style.fontStyle = "italic"
+};
+
--- index.js
+++ index.js
@@ -10,43 +10,41 @@
 //  item.removeChild(removeI);
 
 
-var photos = ["love.jpg", "2.jpg","3.jpg" ];
-var imgTag = document.querySelector("img");
+// var photos = ["love.jpg", "2.jpg","3.jpg" ];
+// var imgTag = document.querySelector("img");
 
-var count = 0;
-
-
+// var count = 0;
 
 
 
-function next() {
+
+
+// function next() {
    
-    count++;
+//     count++;
    
-    if(count >= photos.length){
-        count= 0;
-        imgTag.src= photos[count];
+//     if(count >= photos.length){
+//         count= 0;
+//         imgTag.src= photos[count];
 
-    }else{
-        imgTag.src= photos[count];
-    };
-};
+//     }else{
+//         imgTag.src= photos[count];
+//     };
+// };
 
 
 
-function prev() {
-    count--;
+// function prev() {
+//     count--;
    
-    if(count <0){
-        count= photos.length-1;
-        imgTag.src= photos[count];
+//     if(count <0){
+//         count= photos.length-1;
+//         imgTag.src= photos[count];
 
-    }else{
-        imgTag.src= photos[count];
-    };
-};
-
-
+//     }else{
+//         imgTag.src= photos[count];
+//     };
+// };
 
 
 
